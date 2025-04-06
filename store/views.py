d2861f2ab618c359687f03e874cb08c47743047c
Mamuka27 <mamukagobejishvili3@gmail.com>
2025-04-06 16:41:33 +0400

    Update views.py

--- store/views.py
+++ store/views.py
@@ -39,22 +39,18 @@ def product_search(request):
     category_filter = request.GET.get('category')
     price_min = request.GET.get('price_min')
     price_max = request.GET.get('price_max')
-
     products = Product.objects.all()
-
+    
     if query:
         products = products.filter(Q(name__icontains=query) | Q(slug__icontains=query))
-    
     if category_filter:
         products = products.filter(category__name=category_filter)
-    
     if price_min:
         products = products.filter(price__gte=price_min)
     if price_max:
         products = products.filter(price__lte=price_max)
 
     categories = Category.objects.all()
-
     return render(request, 'product_search.html', {
         'products': products,
         'categories': categories,
